b69e3804a09da68780078190376adaf0ffc4c7b1
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-23 01:20:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,7 +217,7 @@ def most_points_scored
   most_player = ""
   most_points = 0
   game_hash.each do |location, data|
-    data[:players]each do |player, stat|
+    data[:players].each do |player, stat|
       binding.pry
       if stat[:points] > most_points
         most_points = stat[:points]
